a971dc373e6aca7bb8506d8d6d330f2e0b076b84
archanaagivale30 <archana.agivale@qed42.com>
2024-07-22 19:07:47 +0530

    first commit

---
+++ app/sitemap.ts
@@ -0,0 +1,28 @@
+import { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://acme.com',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+      images:['https://example.com/image.jpg']
+    },
+    {
+      url: 'https://acme.com/about',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://acme.com/blog',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+  ]
+}
+export async function generateStaticParams() {
+  return []
+}
